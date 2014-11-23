79604c30028f1c4786572ce3e83c7d6ae96a3c8d
Gregg Thomason <gregg.thomason@asti-usa.com>
2014-11-22 20:27:01 -0500

    in redis, store in a hash.

--- flask_passwordless/token_store.py
+++ flask_passwordless/token_store.py
@@ -1,6 +1,4 @@
 import abc
-from pymongo import MongoClient
-import redis
 
 
 class TokenStore(object):
@@ -37,24 +35,30 @@ class MemoryTokenStore(TokenStore):
 
 class RedisTokenStore(TokenStore):
 
-    def __init__(self, host):
+    def __init__(self, host, tokenspace):
+        import redis
+        if not host:
+            self.host = localhost
+        if not tokenspace:
+            self.tokenspace = 'logintokens'
         self.Redis = redis.StrictRedis(host=host)
-
+        
     def store_or_update(self, token, userid, ttl=600, origin=None):
         # set TTL on key?
-        self.Redis.set(userid, token)
+        self.Redis.hset(self.tokenspace, userid, token)
 
     def invalidate_token(self, userid):
-        self.Redis.delete(userid)
+        self.Redis.hdel(self.tokenspace, userid)
 
     def get_by_userid(self, userid):
-        return self.Redis.get(userid, None)
+        return self.Redis.hget(self.tokenspace, userid, None)
 
 
 class MongoTokenStore(TokenStore):
     STORE = {}
 
     def __init__(self, config):
+        from pymongo import MongoClient
         self.db = MongoClient(config['url'])
         self.origin = config['origin']
         self.ttl = config['ttl']
