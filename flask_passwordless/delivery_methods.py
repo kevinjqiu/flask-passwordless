4d27a512fdf9bbdfc9e764100c71aa1f212a764f
J. Gregg Thomason <gregg.thomason@asti-usa.com>
2014-12-15 12:52:25 -0500

    more template fix

--- flask_passwordless/delivery_methods.py
+++ flask_passwordless/delivery_methods.py
@@ -7,6 +7,7 @@ except ImportError:
     pass
 from templates import MessageTemplate
 import smtplib
+from email.mime.text import MIMEText
 
 
 class DeliveryMethod(object):
@@ -66,20 +67,19 @@ class DeliverByMandrill(DeliveryMethod):
         except mandrill.Error as e:
             raise DeliveryError(str(e))
 
-# TODO: This needs to use Jinja2 templates
+
 class DeliverBySMTP(DeliveryMethod):
     def __init__(self, config):
         """send by smtp"""
         self.tmpl_config = config['TEMPLATES']
         self.config = config['SMTP']
+
+    def __call__(self, token, email):
+        """send the login token"""
         self.from_email = self.config.get('FROM_EMAIL')
         self.msg_subject = self.config.get('MESSAGE_SUBJECT')
         self.server = smtplib.SMTP(self.config.get('SERVER'))
         self.message_template = MessageTemplate(self.tmpl_config)
-
-    def __call__(self, token, email):
-        """send the login token"""
-        from email.mime.text import MIMEText
         fromaddrs = self.from_email
         messagetext = self.message_template(token=token)
         target_email = email + '@' + self.config.get('OK_DOMAIN')
